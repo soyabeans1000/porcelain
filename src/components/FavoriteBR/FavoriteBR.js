0e4afb5fa82a396168767d05af4c1f345ae246a6
Michael Anderson <mjanders6@gmail.com>
2019-05-11 13:46:19 -0700

    initialize and add modules

---
+++ src/components/FavoriteBR/FavoriteBR.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+// Need to create a heart icon
+function FavoriteBR() {
+    return (
+        <Nav.Link href="#Favorite">Favorite</Nav.Link>
+    )
+}
+
+export default FavoriteBR;
