1b323ee26b3410e1439b202c00a54027fe35c847
Michael Anderson <mjanders6@gmail.com>
2019-05-25 13:26:06 -0700

    testing file name change

---
+++ testThingy/src/components/BRAroundMe/BRAroundMe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+// Need to create a Home icon
+function BRAroundMe() {
+    return (
+        <Nav.Link href="#AroundMe">BR Around Me</Nav.Link>
+    )
+}
+
+export default BRAroundMe;
