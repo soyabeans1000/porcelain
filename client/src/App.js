90ffd3f3ca9d1e9c0bc72d2e977484951781755f
Michael Anderson <mjanders6@gmail.com>
2019-05-30 18:34:45 -0700

    fix App page and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import './App.css'
 
 import NavBar from './components/NavBar/navbar'
 import HeaderBar from './components/Header/header'
-import AddBR from './pages/AddBR'
-import BRAroundMe from './components/BRAroundMe/BRAroundMe'
-import Home from './components/Home/Home'
-import Profile from './pages/Profile'
 
-const App = _ => 
-  <div className="App">
-    {/* header component */}
-    <HeaderBar name='Porcelain' />
-    {/* React Reoutes for components. This is coming from the navbar */}
-    <Router>
-      <Route exact path="/" component={_ => <Home />} />
-      <Route path="/AddBR" component={_ => <AddBR />} />
-      <Route path="/AroundMe" component={_ => <BRAroundMe/>} />
-      <Route path="/Profile" component={_ => <Profile />} />
-    </Router>
-    {/* NavBar for views */}
-    <NavBar />
-  </div>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* header component */}
+        <HeaderBar />
+        {/* React Reoutes for components. This is coming from the navbar */}
+         {this.children}
+        {/* NavBar for views */}
+        <NavBar />
+      </div>
+    )
+  }
+}
 
 
 export default App
