2fcf34665c0151cb3f1e56612986566c930e9315
kaylasf <sanfilippokayla@gmail.com>
2019-05-30 21:27:51 -0700

    log in change

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
-
+import {Button} from 'reactstrap'
 const HeaderBar = () => (
-            <Navbar bg="light" expand="lg" fixed="top">
-                <Navbar.Brand href="#home">Porcelain</Navbar.Brand>
-            </Navbar>
+    <>
+    <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar.Brand href="#home">Porcelain</Navbar.Brand>
+       {this.props.loggedIn ? <Button outline color="secondary" onClick={this.props.handleLogoutClick}>Log Out</Button> : <Button outline color="secondary" onClick={this.props.handleLoginClick}>Log In</Button> } 
+    </Navbar>
+    
+    </>
         )
 
-    export default HeaderBar;
+
+export default HeaderBar;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import Login from '../Header/form/login'
 // Need to create a add icon
 
-const Home = _ =>
-    <div>
+class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loggedIn: this.props.isLoggedIn
+        }
+    }
+    render(){
+        return (
+        <div>
+        {this.state.loggedIn == false ? <Login updateLoginStatus={this.props.updateLoginStatus} />: null }
         <h1>Home View</h1>
-    </div>
+        </div>
+        )
+    }
+
+}
+    
 
 export default Home;
