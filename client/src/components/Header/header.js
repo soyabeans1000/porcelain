bc31d29ea2acf296ee9cf505641e6ef1ed424ca3
Sarika <sarika@ngoyal.com>
2019-06-01 18:33:47 -0700

    Logo

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -1,18 +1,31 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
-import {Button} from 'reactstrap'
+import { Navbar, Button } from 'react-bootstrap'
+import logo from '../../images/Logo.png'
+
 
 const HeaderBar = (props) => (
     <>
-    <Navbar bg="light" expand="lg" fixed="top">
-        <Navbar.Brand href="#home">Porcelain</Navbar.Brand>
+         <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar.Brand href="#home"><img src={logo} className="main_logo"></img></Navbar.Brand>
        {props.loggedIn ? <Button outline color="secondary" onClick={_=>{
            props.updateLoginStatus()
            localStorage.removeItem('userId')
            }}>Log Out</Button> : null } 
     </Navbar>
-    
+      
     </>
         )
 
 export default HeaderBar;   
+
+
+
+
+
+// <Navbar bg="light" expand="lg" fixed="top">
+//         <Navbar.Brand href="#home">Porcelain</Navbar.Brand>
+//        {props.loggedIn ? <Button outline color="secondary" onClick={_=>{
+//            props.updateLoginStatus()
+//            localStorage.removeItem('userId')
+//            }} className="navbar-right">Log Out</Button> : null } 
+//     </Navbar>
