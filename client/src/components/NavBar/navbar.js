73efe4abd7a01ef66af7e0bfc47d7ddb3e84cd93
kaylasf <sanfilippokayla@gmail.com>
2019-06-04 20:46:00 -0700

    nav alignment

--- client/src/components/NavBar/navbar.js
+++ client/src/components/NavBar/navbar.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react'
-import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { FaPlusSquare, FaLocationArrow, FaToilet } from 'react-icons/fa'
 // import { FaLocationArrow } from 'react-icons/fa'
@@ -12,22 +12,24 @@ class NavBar extends Component {
     render() {
         return (
             <div className="App-intro">
-                <Navbar expand="lg" fixed="bottom" style={{
-                    flex: 3,
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                }} >
+                <Navbar expand="lg" fixed="bottom" >
                     {/* <Navbar.Brand href="#home">{props.name}</Navbar.Brand> */}
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav expand="lg" fixed="bottom" >
-                            <Container>
-                                <Row>
-                                    {/* NavBar links  */}
-                                    <Nav.Link> <Link  to="/AroundMe">Around Me<FaLocationArrow /></Link> </Nav.Link>
-                                    <Nav.Link> <Link  to="/AddBR">Add Bathroom<  FaPlusSquare /></Link> </Nav.Link>
-                                    <Nav.Link> <Link  to="/Profile">Profile<FaToilet /></Link> </Nav.Link>
-
+                    <Navbar.Collapse id="basic-navbar-nav"  >
+                        <Nav expand="lg" fixed="bottom" style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+      }} >
+                            <Container >
+                                <Row >
+                                    
+                                        {/* NavBar links  */}
+                                       <Col> <Nav.Link> <Link to="/AroundMe">Around Me<FaLocationArrow /></Link> </Nav.Link></Col>
+                                       <Col> <Nav.Link> <Link to="/AddBR">Add Bathroom<  FaPlusSquare /></Link> </Nav.Link></Col>
+                                       <Col> <Nav.Link> <Link to="/Profile">Profile<FaToilet /></Link> </Nav.Link> </Col>
+                                  
                                 </Row>
                             </Container>
                         </Nav>
