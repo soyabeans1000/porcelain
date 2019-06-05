b33456b465cd64245584c0132704a3b8645f268b
Jonathan Lui <jonathanlui@dhcp-v083-175.mobile.uci.edu>
2019-06-04 20:26:16 -0700

    deleted some comments.

--- client/src/components/bathroom/bathroom.js
+++ client/src/components/bathroom/bathroom.js
@@ -20,7 +20,6 @@ const Bathroomform = ({bathroom, handleLikebutton, likecount, newcomment, handle
                     {comments.sort(function(a, b){
                         var keyA = new Date(a.createdAt),
                             keyB = new Date(b.createdAt);
-                        // Compare the 2 dates
                         if(keyA < keyB) return -1;
                         if(keyA > keyB) return 1;
                         return 0;
@@ -28,20 +27,13 @@ const Bathroomform = ({bathroom, handleLikebutton, likecount, newcomment, handle
                         <div>
                             <span>{username}: </span>
                             <span>{comment}</span>
-                            {/* access to the logged in username/id */}
-                    
                            { loggedInUser === userId ? <button id={id} value={index} onClick={handledelete}>X</button> : null }
-
-                    
-                            {/* <button type="submit" onClick={handledelete}>Delete</button> */}
                         </div>
                     ))}
                 </div>
                 <div>
                     <form className="comment-form" id="commentform">
                         <div className="comment-form-fields">
-                                {/* need to pull in username from sign-in */}
-                                {/* <input placeholder="Name" required ref={(input) => this._author = input}></input><br /> */}
                             <textarea placeholder="Comment" rows="2" id="newcomment" value={newcomment} onChange={handleInputChange}></textarea>
                         </div>
                         <div className="comment-form-actions">
