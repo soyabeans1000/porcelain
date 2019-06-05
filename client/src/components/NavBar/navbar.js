ffc25ab8592d979c7fad02f062ba34123e2e3c8d
kaylasf <sanfilippokayla@gmail.com>
2019-06-04 19:22:00 -0700

    nothing

--- client/src/components/NavBar/navbar.js
+++ client/src/components/NavBar/navbar.js
@@ -2,23 +2,31 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { FaPlusSquare, FaLocationArrow, FaToilet } from 'react-icons/fa'
+// import { FaLocationArrow } from 'react-icons/fa'
+
+
 
 
 class NavBar extends Component {
     render() {
         return (
             <div className="App-intro">
-                <Navbar bg="light" expand="lg" fixed="bottom">
+                <Navbar expand="lg" fixed="bottom" style={{
+                    flex: 3,
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                }} >
                     {/* <Navbar.Brand href="#home">{props.name}</Navbar.Brand> */}
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav expand="lg" fixed="bottom" >
                             <Container>
                                 <Row>
                                     {/* NavBar links  */}
-                                    <Nav.Link> <Link to="/AroundMe">Around Me</Link> </Nav.Link>
-                                    <Nav.Link> <Link to="/AddBR">Add Bathroom</Link> </Nav.Link>
-                                    <Nav.Link> <Link to="/Profile">Profile</Link> </Nav.Link>
+                                    <Nav.Link> <Link  to="/AroundMe">Around Me<FaLocationArrow /></Link> </Nav.Link>
+                                    <Nav.Link> <Link  to="/AddBR">Add Bathroom<  FaPlusSquare /></Link> </Nav.Link>
+                                    <Nav.Link> <Link  to="/Profile">Profile<FaToilet /></Link> </Nav.Link>
 
                                 </Row>
                             </Container>
